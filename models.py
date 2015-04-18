a7453c442767b3813f21c47e81285c9f572dfe8f
AvaniPai <AvaniPai@wirelessprvnat-172-17-51-239.near.illinois.edu>
2015-04-18 15:49:04 -0500

    updated models.py

--- models.py
+++ models.py
@@ -9,6 +9,11 @@ class User(db.Model):
 	username = db.Column(db.String())
 	email = db.Column(db.String())
 	password = db.Column(db.String())
+	
+	def __init__(self, username, email, password):
+		self.username = username
+		self.email = email
+		self.password = password
 
 	def __repr__(self):
 		return "<User(id='%s',username='%s')>" % (str(self.id), self.username)
