efee01bd6c8890286c5d5dae243712c4d7507a64
AvaniPai <avanipai695@gmail.com>
2015-04-26 18:16:55 -0500

    hopefully this works

--- models.py
+++ models.py
@@ -9,8 +9,9 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String())
 	email = db.Column(db.String())
-	password = db.Column(db.String())
-	
+	password = db.Column(db.String())	
+	personalize = relationship("Preferences", order_by="Preferences.id", backref="user")
+
 	def __init__(self, username, email, password):
 		self.username = username
 		self.email = email
@@ -19,12 +20,14 @@ class User(db.Model):
 	def __repr__(self):
 		return "<User(id='%s',username='%s')>" % (str(self.id), self.username)
 
-class Preferences(db.Model)
+class Preferences(db.Model):
+
 	__tablename__ = 'preferences'
+
 	id = db.Column(db.Integer, primary_key=True)
-	decisionType = db.Column(db.String(), nullable=False)
-	option= db.Column(db.String(), nullable=False)
-	sadPref =db.Column(db.Integer)
+	decisionType = db.Column(db.String())
+	option= db.Column(db.String())
+	sadPref = db.Column(db.Integer)
 	happyPref = db.Column(db.Integer)
 	angryPref = db.Column(db.Integer)
 	excitedPref = db.Column(db.Integer)
@@ -32,7 +35,7 @@ class Preferences(db.Model)
 	boredPref = db.Column(db.Integer)
 	user_id = db.Column(db.Integer, ForeignKey('userinfo.id'))
 
-	user = relationship("User", backref=backref('preferences', order_by=id))
+	user = relationship("User", backref=backref('personalize', order_by=id))
 	
 	def __init__(self, types, option, mood, value):
 		self.decisionType=types
@@ -41,12 +44,14 @@ class Preferences(db.Model)
 			self.happyPref = value
 		elif mood == "angry":
 			self.angryPref = value
-		elif mood == "sad"
+		elif mood == "sad":
 			self.sadPref = value
 		elif mood == "excited":
 			self.excitedPref = value
 		elif mood == "bored":
 			self.boredPref = value
-
+		elif mood == "tired":
+			self.tiredPref = value
+	
 	def __repr__(self):
-		return "<Preference(type='%s',option='%s',value='%s')>" % (self.decisionType,self.option,(str)self.happyPref) 	
+		return "<Preference(type='%s',option='%s',value='%s')>" % (self.decisionType,self.option,str(self.happyPref)) 	
