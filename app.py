82a361513d2fc0b29eb3482aefccc1cc67a0f9f5
AvaniPai <avanipai695@gmail.com>
2015-04-26 02:11:53 -0500

    pulling value attempt 9

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def make_connection():
 	if request.method == "POST":
 		var = request.get_json(force=True)
 		secret = var['password']
-		address = var['email']
-		return "Hi there %!" + str(var) + str(address)
+		address = var['country']
+		person = var['username']
+		return "Hi there %!" + str(var) + str(address) + str(person)
 
