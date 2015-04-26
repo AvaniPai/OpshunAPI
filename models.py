6b85435c1cb77b346708a158a0fc8df3ffc36416
AvaniPai <avanipai695@gmail.com>
2015-04-26 16:43:29 -0500

    trying to update our database by adding preferences table

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from app import db
-
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 class User(db.Model):
 
@@ -17,3 +18,20 @@ class User(db.Model):
 
 	def __repr__(self):
 		return "<User(id='%s',username='%s')>" % (str(self.id), self.username)
+
+class Preference(db.Model):
+	__tablename__ = 'preferences'
+	id = db.Column(db.Integer, primary_key=True)
+	decisionType = db.Column(db.String(), nullable=False)
+	option= db.Column(db.String(), nullable=False)
+	sadPref =db.Column(db.Integer)
+	happyPref = db.Column(db.Integer)
+	angryPref = db.Column(db.Integer)
+	excitedPref = db.Column(db.Integer)
+	tiredPref = db.Column(db.Integer)
+	boredPref = db.Column(db.Integer)
+	user_id = db.Column(db.Integer foreign_key(('userinfo.id'))
+
+	user = relationship("User", backref=backref('preferences', order_by=id))
+	
+	
